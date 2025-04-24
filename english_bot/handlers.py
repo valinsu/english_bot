f443624e29d9369f06744c3ac50dd5090526d3f1
valinsu <veronikabalbes@gmail.com>
2025-04-24 20:09:32 +0300

    Add os from path lessons/word

--- english_bot/handlers.py
+++ english_bot/handlers.py
@@ -2,6 +2,7 @@ from telegram import Update
 from telegram.ext import ContextTypes
 import json
 import random
+import os
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hi bro, let's learn some English words together!")
@@ -9,8 +10,12 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def word_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
-        with open("english_bot\lessons\words.json", "r", encoding="utf-8") as f:
+        current_dir = os.path.dirname(__file__)
+        path_to_json = os.path.join(current_dir, "lessons", "words.json")
+        
+        with open(path_to_json, "r", encoding="utf-8") as f:
             words = json.load(f)
+    
         word_data = random.choice(words)
         response = f"Word: *{word_data['word']}*\nRU Translation: {word_data['translation']}\nExample: _{word_data['example']}_"
         await update.message.reply_markdown_v2(response)
